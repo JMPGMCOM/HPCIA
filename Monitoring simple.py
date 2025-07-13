1abb11fce086d9311ccf04f93759274d7b383de1
JMPGMCOM <julienmontaigupro@gmail.com>
2025-07-13 14:28:44 +0200

    Update Monitoring simple.py
    Ajout de commentaires

--- Monitoring simple.py
+++ Monitoring simple.py
@@ -2,13 +2,13 @@ import psutil
 import time
 
 def monitor_resources():
+    # Mesure l'utilisation du CPU sur 1 seconde
     cpu = psutil.cpu_percent(interval=1)
+    # Récupère le pourcentage de mémoire vive utilisée
     memory = psutil.virtual_memory().percent
+    # Récupère le pourcentage d'espace disque utilisé sur la racine ('/')
     disk = psutil.disk_usage('/').percent
+    # Retourne les mesures sous forme de dictionnaire, avec un timestamp (date/heure en secondes depuis l'époque Unix)
     return {
         'cpu': cpu,
-        'memory': memory,
-        'disk': disk,
-        'timestamp': time.time()
-    }
-print(monitor_resources())
+        'memory': memory
